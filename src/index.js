b48fb7485786203f752d0d8de0bea4b4401aa6c5
SandraNthanje <sandrasandies13@gmail.com>
2024-03-01 21:11:44 +0200

    Added api functionality to the search engine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    
 
 }
 
 
-let searchFormElement=document.querySelector("#search-form");
+
+
+function searchCity(city) {
+    let apiKey = "021aac44e43f5dot276a89e05bb0245d";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+
+}
+
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    
+   
+    searchCity(searchInput.value);
+
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
